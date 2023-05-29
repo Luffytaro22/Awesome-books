92482423f5dc747c9aaa90a2bd528e21b900cc8c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-29 15:00:17 -0500

    Added a validity condition and saving in local storage

--- storage.js
+++ storage.js
@@ -2,15 +2,21 @@ const mainContainer = document.querySelector('#books-container');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const submitButton = document.querySelector('#book-submit');
+const form = document.querySelector('#form-book');
 
 let books = [];
+
+if (localStorage.getItem('library')) {
+  /* Reasign the books array to the localStorage library */
+  books = JSON.parse(localStorage.getItem('library'));
+} 
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
 }
 
-function createBook() {
+function createBook(book) {
   /* Create elements */
   const bookContainer = document.createElement('div');
   const pTitle = document.createElement('p');
@@ -19,11 +25,12 @@ function createBook() {
 
   /* Add Classes and properties */
   bookContainer.classList.add('books');
-  removeButton.tabIndex = books.indexOf(Book.title);
+  removeButton.tabIndex = books.indexOf(book);
 
   /* Add text */
-  pTitle = Book.title;
-  pAuthor = Book.author;
+  pTitle.textContent = book.title;
+  pAuthor.textContent = book.author;
+  removeButton.textContent = 'Remove';
 
   /* Append elements */
   bookContainer.appendChild(pTitle);
@@ -32,18 +39,33 @@ function createBook() {
   mainContainer.appendChild(bookContainer);
 }
 
-function addBook() {
+books.forEach((book) => {
+  createBook(book);
+});
+
+function addBook(event) {
+  /* Create a new book */
   const book = new Book(titleInput.value, authorInput.value);
-  
+
+  /* If the inputs has no values then form is not submmited */
+  if (!titleInput.validity.valueMissing && !authorInput.validity.valueMissing) {
+    event.preventDefault();
+  } else {
+    return;
+  }
+
   /* If the library key exists */
   if (localStorage.getItem('library')) {
     /* Copy the library key to the books array */
-    books = JSON.parse(localStorage.getItem('library'));
-    localStorage.setItem('libray', JSON.stringify(books));
+    books.push(book);
+    localStorage.setItem('library', JSON.stringify(books));
   } else {
-    /* Just create a new library key */
-    localStorage.setItem('libray', JSON.stringify(books));
+    books.push(book);
+    localStorage.setItem('library', JSON.stringify(books));
   }
+
+  form.reset(); 
+  createBook(book);
 }
 
-submitButton.add('click', addBook);
+submitButton.addEventListener('click', addBook);
