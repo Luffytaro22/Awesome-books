4a66497b1a0ec43e013cefa73789d949053fceba
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-05-31 10:16:16 -0500

    Added event listener to remove and add the "hide" class

--- date-menu.js
+++ date-menu.js
@@ -1,5 +1,33 @@
 const pDate = document.querySelector('#date');
+const list = document.querySelector('.nav-sections:nth-child(1)');
+const addNew = document.querySelector('.nav-sections:nth-child(2)');
+const contact = document.querySelector('.nav-sections:nth-child(3)');
+const booksSection = document.querySelector('#books-section');
+const formSection = document.querySelector('#addBook-section');
+const contactSection = document.querySelector('#contact');
 
 const today = new Date();
 
 pDate.innerHTML = today;
+
+function showForm() {
+  booksSection.classList.add('hide');
+  formSection.classList.remove('hide');
+  contactSection.classList.add('hide');
+}
+
+function showContact() {
+  booksSection.classList.add('hide');
+  formSection.classList.add('hide');
+  contactSection.classList.remove('hide');
+}
+
+function showBooks() {
+  booksSection.classList.remove('hide');
+  formSection.classList.add('hide');
+  contactSection.classList.add('hide');
+}
+
+list.addEventListener('click', showBooks);
+addNew.addEventListener('click', showForm);
+contact.addEventListener('click', showContact);
